4cf49256e914e6fd92027f22aedce481c8969d5f
bu6hunt3r <bu6hunt3r@web.de>
2023-11-28 14:23:55 +0100

    removed crafted-emacs config

--- ansible/dotfiles/crafted-emacs/.crafted-emacs/elpa/sotlisp-1.6.2/sotlisp-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from sotlisp.el  -*- no-byte-compile: t -*-
-(define-package "sotlisp" "1.6.2" "Write lisp at the speed of thought." '((emacs "24.1")) :keywords '("convenience" "lisp") :authors '(("Artur Malabarba" . "emacs@endlessparentheses.com")) :maintainer '("Artur Malabarba" . "emacs@endlessparentheses.com") :url "https://github.com/Malabarba/speed-of-thought-lisp")
