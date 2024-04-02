3f21124385e34a61c83b05f7cb7db6cd219371cf
bu6hunt3r <bu6hunt3r@web.de>
2024-04-02 16:32:32 +0200

    Added prayer times configuration for waybar etc.

--- ansible/dotfiles/prayers/.local/bin/prayer-times-yad.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yad --no-buttons --text "<span font-size='large'>$($HOME/.local/bin/prayers.sh)</span>" --title "Prayers"
--- ansible/dotfiles/prayers/.local/bin/prayer.sh
+++ ansible/dotfiles/prayers/.local/bin/prayer.sh
@@ -7,64 +7,60 @@ prayers="$HOME/.local/share/prayers.json"
 # Get the current time
 currenttime=$(date +%s)
 day=$(date +%a)
-day_idx=$(( $(date +%d | awk '/^0.*/{sub("0","")}{print}') - 1 ))
+day_idx=$(($(date +%-d) - 1))
 
 # Parsing the data for the five salawat
 # use epoch seconds in order to calculate time difference
-# date -d $(jq ".data[DAY - 1].timings.PRAYER" $prayers | bc) +%s
-fajr=$(date -d "$(jq ".data[$day_idx].timings.Fajr" $prayers | bc)" +%s)
-dhuhr=$(date -d "$(jq ".data[$day_idx].timings.Dhuhr" $prayers | bc)" +%s)
-asr=$(date -d "$(jq ".data[$day_idx].timings.Asr" $prayers | bc)" +%s)
-maghrib=$(date -d "$(jq ".data[$day_idx].timings.Maghrib" $prayers | bc)" +%s)
-isha=$(date -d "$(jq ".data[$day_idx].timings.Isha" $prayers | bc)" +%s)
+# date -d $(jq -r ".data[DAY - 1].timings.PRAYER" $prayers) +%s
+fajr=$(date -d "$(jq -r ".data[$day_idx].timings.Fajr" "$prayers")" +%s)
+dhuhr=$(date -d "$(jq -r ".data[$day_idx].timings.Dhuhr" "$prayers")" +%s)
+asr=$(date -d "$(jq -r ".data[$day_idx].timings.Asr" "$prayers")" +%s)
+maghrib=$(date -d "$(jq -r ".data[$day_idx].timings.Maghrib" "$prayers")" +%s)
+isha=$(date -d "$(jq -r ".data[$day_idx].timings.Isha" "$prayers")" +%s)
 
+if [[ "$currenttime" -ge "$fajr" && "$currenttime" -lt "$dhuhr" ]]; then
+	nexttime=$dhuhr
+	currentprayer="Fajr"
+	if [[ "$day" == "Fri" ]]; then
+		nextprayer="Jumuaa"
+	else
+		nextprayer="Dhuhr"
+	fi
 
-if [ $currenttime -ge $fajr ] && [ $currenttime -lt $dhuhr ]; then
-    nexttime=$dhuhr
-    currentprayer="Fajr"
-    if [[ "$day" == "Fri" ]]; then
-        nextprayer="Jumuaa"
-    else
-            nextprayer="Dhuhr"
-    fi
+elif [[ $currenttime -ge $dhuhr && $currenttime -lt $asr ]]; then
+	nexttime=$asr
+	nextprayer="Asr"
+	currentprayer="Dhuhr"
 
-elif [ $currenttime -ge $dhuhr ] && [ $currenttime -lt $asr ]; then
-    nexttime=$asr
-    nextprayer="Asr"
-    currentprayer="Dhuhr"
+elif [[ $currenttime -ge $asr && $currenttime -lt $maghrib ]]; then
+	nexttime=$maghrib
+	nextprayer="Maghrib"
+	currentprayer="Asr"
 
-elif [ $currenttime -ge $asr ] && [ $currenttime -lt $maghrib ]; then
-    nexttime=$maghrib
-    nextprayer="Maghrib"
-    currentprayer="Asr"
+elif [[ $currenttime -ge $maghrib && $currenttime -lt $isha ]]; then
+	nexttime=$isha
+	nextprayer="Isha"
+	currentprayer="Maghrib"
 
-elif [ $currenttime -ge $maghrib ] && [ $currenttime -lt $isha ]; then
-    nexttime=$isha
-    nextprayer="Isha"
-    currentprayer="Maghrib"
-
-elif [ $currenttime -ge $isha ] || [ $currenttime -lt $fajr ]; then
-    nexttime=$fajr
-    nextprayer="Fajr"
-    currentprayer="Isha"
+elif [[ $currenttime -ge $isha || $currenttime -lt $fajr ]]; then
+	nexttime=$fajr
+	nextprayer="Fajr"
+	currentprayer="Isha"
 fi
 
-# Calculate the remaining time to the next prayer (or iftar in ramadan and the fast duration is ramadan)
-remain=$(date -u -d "@$(( "$nexttime" - "$currenttime" ))" "+%H:%M")
-
-# Ramadan timings
-# fast=$(date -u -d "@$(( "$maghrib" - "$fajr" ))" '+%H:%M')
-# Tofast=$(date -u -d "@$(( "$maghrib" - "$currenttime" ))" '+%H:%M')
+# Calculate the remaining time to the next prayer
+remain=$(date -u -d "@$((nexttime - currenttime))" "+%H:%M")
+# ------ Ramadan Timings ------ #
+# fast=$(date -u -d "@$(( maghrib - fajr ))" '+%H:%M')
+# Tofast=$(date -u -d "@$(( maghrib - currenttime ))" '+%H:%M')
 
 # ======================= OUTPUT ======================= #
+#current_out="$currentprayer ($remain)"
+next_out="$nextprayer in $remain"
 
-# Get the current fard
-#printf "$currentprayer ($remain)"
-
-nexttime=$(date -u -d "@$nexttime" "+%H:%M")
 # Get the next fard
 if [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
-  printf "$nextprayer in $remain"
+	echo -n "$next_out"
 else
-  printf '{ "text": "%s", "class": "%s" }\n' "$nextprayer: $nexttime" "$nextprayer"
+	printf '{ "text": "%s", "class": "%s" }\n' "$next_out" "$nextprayer"
 fi
